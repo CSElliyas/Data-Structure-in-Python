17391b14fb090ebcfc54e410aef35b39f200ebb5
Elliyas Ahmed <elliyasahmedbd@gmail.com>
2017-08-26 22:59:35 +0600

    Traversing Linear Arrays With For Loop in Python
    Traversing Linear Arrays With For Loop in Python

---
+++ traversing_with_for.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#This program will print the element of list LA where K is a counter with For Loop.
+#PROGRAM BY ELLIYAS AHMED
+
+LA = [2, 1, 3, 4, 5] #in Python list works as array in C
+
+for K in range(0,len(LA)):
+	print LA[K]
+print 'End of Traversing' #PRINT END OF TRAVERSING
