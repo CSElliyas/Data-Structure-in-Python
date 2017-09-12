0461ed96831caafe5087a40ed42c2ebe04fa1c3b
Elliyas Ahmed <elliyasahmedbd@gmail.com>
2017-09-12 11:14:19 +0600

    Some edition

--- TOH.py
+++ TOH.py
@@ -3,6 +3,7 @@
 #Tower of Hanoi Problem with Python
 #By Elliyas Ahmed
 #12.09.2017
+#www.compromath.com
 #Method Recursive
 def TOH(n, fromTower, withTower, toTower):
 	if n>0:
@@ -11,6 +12,8 @@ def TOH(n, fromTower, withTower, toTower):
 		TOH(n-1, withTower, fromTower, toTower)
 
 def moveDisk(fromT, toT):
-	print("Move Disk from", fromT, "-", toT)
+	print("Move Disk from", fromT, "to", toT)
 
-TOH(3, '1', '2', '3')
+TOH(3, 'A', 'B', 'C')
+#Here A, B, C the number of Tower
+#3 is the number of Disk
